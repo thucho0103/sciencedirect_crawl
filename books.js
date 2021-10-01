85ac357b1c2ab75bbba964a723500227bca6f112
thucduy0103 <thucduy0103@gmail.com>
2021-10-01 20:56:37 +0700

    update

---
+++ books.js
@@ -0,0 +1,42 @@
+const axios = require('axios')
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+const download = require('download');
+
+const filePath = `${__dirname}/files`;
+
+const count = 100
+
+axios.get(`https://api.elsevier.com/content/search/sciencedirect?start=0&count=${count}&query=artificial%20intelligence%20marketing+and+openaccess%281%29&apiKey=7f59af901d2d86f78a1fd60c1bf9426a&httpAccept=application%2Fjson`)
+.then((response) => {
+
+    const result = response.data['search-results'].entry
+
+    result.forEach(element => {
+        console.log(element['prism:doi']);
+
+        axios.get(`https://sci-hub.se/${element['prism:doi']}`)
+        .then((response) => {
+            let $ = cheerio.load(response.data)
+            let link =  $('#buttons > button').attr('onclick')
+            let linkDownload = link.slice(link.indexOf("sci") ,link.length-1)
+            console.log(linkDownload);
+            
+            download(`https://${linkDownload}`,filePath)
+            .then(() => {         
+                console.log('Download Completed');
+            })
+        })
+        .catch(function (e) {
+            console.log("error " + e.message);
+            res.render('index', { data: [] });
+        });
+        
+    });
+
+})
+.catch(function (e) {
+    console.log("error " + e.message);
+    res.render('index', { data: [] });
+});
+
